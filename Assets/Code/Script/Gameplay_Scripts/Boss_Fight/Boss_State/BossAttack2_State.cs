dd8c4bb5b52ede1b6f8a8c5659c4f597172d5a7e
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-12-01 18:03:49 +0100

    attack 1 whitput damage to player

--- Assets/Code/Script/Gameplay_Scripts/Boss_Fight/Boss_State/BossAttack2_State.cs
+++ Assets/Code/Script/Gameplay_Scripts/Boss_Fight/Boss_State/BossAttack2_State.cs
@@ -6,10 +6,12 @@ public class BossAttack2_State : BossState
 {
     BossController controller;
     int i;
-    public BossAttack2_State(BossController boss,int i) : base()
+    BossView bossView;
+    public BossAttack2_State(BossController boss,int i,BossView bossView) : base()
     {
         this.controller = boss;
         this.i = i;
+        this.bossView = bossView;
     }
 
 
@@ -21,11 +23,11 @@ public class BossAttack2_State : BossState
     public override void Process()
     {
         Debug.Log("Attack2");
-        controller._animator.SetTrigger("Attack");
-        controller.objectThrown= controller.SpawnObject();
-        Vector3 direction= controller.playerTransform- controller.Test.position;
-        controller.objectThrown.GetComponent<Rigidbody>().velocity = direction*Time.deltaTime*controller.phase_So[i].ProjectileSpeed;
-        controller.StartCoroutine(controller.Cooldown());
+        controller.BossView.Animator.SetTrigger("Attack");
+        controller.ObjectThrown= controller.SpawnObject();
+        Vector3 direction= controller.PlayerPos() - controller.transform.position;
+        controller.ObjectThrown.GetComponent<Rigidbody>().velocity = direction*Time.deltaTime*controller.PhaseSo[i].ProjectileSpeed;
+        controller.StartCoroutine(controller.Attack());
     }
 
     public override void Exit()

4c7e2966c58188df380783e7b205f95ee68eeb84
emacrapa <emanuele.crapanzano02@gmail.com>
2024-12-06 12:32:45 +0100

    collectibles respawn

--- Assets/Code/Script/Gameplay_Scripts/Collectibles/Collectible.cs
+++ Assets/Code/Script/Gameplay_Scripts/Collectibles/Collectible.cs
@@ -1,16 +1,72 @@
 using UnityEngine;
+using System.Collections;
 
 namespace Collectible_System
 {
     public class Collectible : MonoBehaviour
     {
         [SerializeField] private CollectibleType _type;
+        [SerializeField] private bool enableRespawn = false;
+        [SerializeField] private float respawnTime = 5f;
+
+        private Collider _collider;
+        private GameObject _childObject;
+
+        private void Awake()
+        {
+            _collider = GetComponent<Collider>();
+
+            if (transform.childCount > 0)
+            {
+                _childObject = transform.GetChild(0).gameObject;
+            }
+            else
+            {
+                Debug.LogError("No child", this);
+            }
+        }
 
         public void Collect()
         {
-            this.gameObject.SetActive(false);
+            DisableCollectible();
+
+            if (enableRespawn)
+            {
+                StartCoroutine(RespawnAfterDelay(respawnTime));
+            }
+        }
+
+        private void DisableCollectible()
+        {
+            if (_collider != null)
+                _collider.enabled = false;
+
+            if (_childObject != null)
+                _childObject.SetActive(false);
+        }
+
+        private void EnableCollectible()
+        {
+            if (_collider != null)
+                _collider.enabled = true;
+
+            if (_childObject != null)
+                _childObject.SetActive(true);
+        }
+
+        private IEnumerator RespawnAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            EnableCollectible();
         }
 
         public CollectibleType CollectibleType => _type;
     }
+        //public void Collect()
+        //{
+        //    this.gameObject.SetActive(false);
+        //}
+
+        //public CollectibleType CollectibleType => _type;
 }
